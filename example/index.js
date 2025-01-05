2bb0aa9251e759e2ec4e0a75b722b3f16d6b1c8e
SangYong <gkdis6@gmail.com>
2025-01-05 20:53:32 +0900

    edit etc

--- example/index.js
+++ example/index.js
@@ -1,7 +1,3 @@
-import {FlowModule} from "flow-sdk";
+import {Module} from "sdk";
 
-const flowModule = new FlowModule('https://flow.info', 'content-key');
-
-const response = await flowModule.sendBotNotifications('bot_id','rcvr_user_id', '안녕');
-
-console.log(response);
+const module = new Module('https://example.com', 'content-key');
